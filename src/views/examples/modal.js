0edee6f592efa5ea39e6f6fc0cef962b9a8452e6
Valet-V0ult-de-Fur1e <82310399+Valet-V0ult-de-Fur1e@users.noreply.github.com>
2024-12-01 12:45:34 +0300

    test

---
+++ src/views/examples/modal.js
@@ -0,0 +1,68 @@
+import { useEffect, useState } from "react";
+import './tables.css'
+
+// // Компонент для отображения модального окна
+const Modal = ({ isOpen, onClose, onSave, rowData }) => {
+    const getData = ()=> {
+        console.log(rowData)
+        if (rowData === null) return {
+            id: Date.now(),
+            name: '',
+            age: '',
+        }
+        return rowData
+    }
+	const [data, setData] = useState(getData());
+    console.log(data)
+
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		setData((prevData) => ({ ...prevData, [name]: value }));
+	};
+
+	const handleSave = () => {
+		onSave(data);
+		onClose();
+	};
+
+    useEffect(
+        ()=>{
+            if (rowData !== null)
+            if (data.id !== rowData.id) {
+                setData(rowData);
+            }
+        }, [rowData, data.id]
+    )
+
+	return (
+		isOpen && (
+			<div className="modal">
+				<div className="modal-content">
+					<h2>Edit Row</h2>
+					<label>
+						Name:
+						<input
+							type="text"
+							name="name"
+							value={data.name}
+							onChange={handleChange}
+						/>
+					</label>
+					<label>
+						Age:
+						<input
+							type="text"
+							name="age"
+							value={data.age}
+							onChange={handleChange}
+						/>
+					</label>
+					<button onClick={handleSave}>Save</button>
+					<button onClick={onClose}>Close</button>
+				</div>
+			</div>
+		)
+	);
+};
+
+export default Modal;
